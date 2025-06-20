97c86dca7d6a95fd4e0cac782ea16bc0e9a7f93c
Sabina KAlchuk <ssabki229@gmail.com>
2025-06-20 22:59:46 +0200

    ts

---
+++ src/task-7.ts
@@ -0,0 +1,10 @@
+function getMessage() {
+    return new Promise<string>((resolve) => {
+      setTimeout(() => {
+        resolve("Hello from TS");
+      }, 1000);
+    });
+  }
+  
+  getMessage().then(result => console.log(result));
+  
