36ac974745d9602af73d4ec86be312c5ea3782ff
Sabina KAlchuk <ssabki229@gmail.com>
2025-06-21 11:19:03 +0200

    hw2

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,11 +1,11 @@
-interface Product {
+
+  interface Product {
     readonly id: number;
     title: string;
     description?: string;
-}
-
-
-const product: Product = {
+  }
+  
+  const product: Product = {
     id: 1,
     title: "Tablet",
     description: "Compact and fast",
